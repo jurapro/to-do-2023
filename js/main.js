df63f27684d0336b25174dff91a037d5a41f2bb0
jurapro <jurapro@yandex.ru>
2023-03-19 01:40:53 +0700

    done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,45 @@
+Vue.component('add-task', {
+    template: `
+    <div class="column">
+        <h2>Добавить задачу</h2>
+        <div>
+        <label>Название задачи <input :value="task.name"></label>
+        <h3>Подзадачи</h3>
+        <div v-for="(subtask, index) in task.subtasks"><input :value="subtask.name" :key="index">
+        <button @click="delSubtask(index)">-</button>
+        </div>
+        <button @click="addSubtask">+</button><button @click="addTask">Добавить</button>
+        </div>
+    </div>
+    `,
+    methods: {
+        addSubtask() {
+            this.task.subtasks.push({name: "Пункт " + (this.task.subtasks.length + 1), done: false})
+        },
+        delSubtask(index) {
+            this.task.subtasks.splice(index, 1)
+        },
+        addTask() {
+            this.$emit('add-task', JSON.parse(JSON.stringify(this.task)))
+        }
+    },
+    data() {
+        return {
+            task: {
+                name: 'Новая задача',
+                subtasks: [
+                    {name: "Пункт 1", done: false},
+                    {name: "Пункт 2", done: false},
+                    {name: "Пункт 3", done: false},
+                ]
+            }
+        }
+    },
+})
+
 Vue.component('column', {
     props: {
         column: {
-            disabled: false,
-            index: null,
             name: '',
             tasks: []
         }
@@ -21,6 +58,9 @@ Vue.component('column', {
         </div>
     </div>
     `,
+    updated() {
+        this.$emit('save')
+    },
     methods: {
         doneSubtask(subtask) {
             this.$emit('done-subtask', subtask)
@@ -30,7 +70,6 @@ Vue.component('column', {
         },
         taskFilledCompletely(task) {
             this.$emit('task-filled-completely', {task: task, column: this.column})
-            this.$emit('column-1-unlock')
         }
     }
 })
@@ -82,6 +121,7 @@ let app = new Vue({
         name: "Великолепное приложение TODO",
         columns: [
             {
+                maxSize: 3,
                 disabled: false,
                 index: 0,
                 name: "Новые задачи",
@@ -96,56 +136,17 @@ let app = new Vue({
                             {name: "Пункт 1.5", done: false},
                         ]
                     },
-                    {
-                        name: "Задача 2",
-                        subtasks: [
-                            {name: "Пункт 2.1", done: false},
-                            {name: "Пункт 2.2", done: false},
-                            {name: "Пункт 2.3", done: false},
-                        ]
-                    },
-                    {
-                        name: "Задача 3",
-                        subtasks: [
-                            {name: "Пункт 3.1", done: false},
-                            {name: "Пункт 3.2", done: false},
-                            {name: "Пункт 3.3", done: false},
-                        ]
-                    }
                 ]
             },
             {
+                maxSize: 5,
                 disabled: false,
                 index: 1,
                 name: "В процессе",
-                tasks: [
-                    {
-                        name: "Задача 3",
-                        subtasks: [
-                            {name: "Пункт 3.1", done: false},
-                            {name: "Пункт 3.2", done: true},
-                            {name: "Пункт 3.3", done: true},
-                        ]
-                    },
-                    {
-                        name: "Задача 3",
-                        subtasks: [
-                            {name: "Пункт 3.1", done: true},
-                            {name: "Пункт 3.2", done: false},
-                            {name: "Пункт 3.3", done: true},
-                        ]
-                    },
-                    {
-                        name: "Задача 3",
-                        subtasks: [
-                            {name: "Пункт 3.1", done: true},
-                            {name: "Пункт 3.2", done: false},
-                            {name: "Пункт 3.3", done: true},
-                        ]
-                    }
-                ]
+                tasks: []
             },
             {
+                maxSize: -1,
                 disabled: false,
                 index: 2,
                 name: "Закончено",
@@ -153,36 +154,56 @@ let app = new Vue({
             }
         ]
     },
+    mounted() {
+        if (!localStorage.getItem('columns')) return
+        this.columns = JSON.parse(localStorage.getItem('columns'));
+    },
     methods: {
+        saveData() {
+            localStorage.setItem('columns', JSON.stringify(this.columns))
+        },
+        addTask(task) {
+            if (this.column1Full || this.columns[0].disabled) return
+            this.columns[0].tasks.push(task)
+        },
         doneSubtask(subtask) {
             subtask.done = true
+            this.saveData()
         },
         taskHalfFilled(data) {
             if (data.column.index !== 0 || data.column.disabled) return
-            if (this.columns[1].tasks.length === 5) {
+            if (this.column2Full) {
                 this.columns[0].disabled = true
                 return;
             }
-            this.moveTask(data)
+            this.moveTask(data, this.columns[1])
         },
         taskFilledCompletely(data) {
-            if (data.column.index !== 1) return
-            this.moveTask(data)
+            this.moveTask(data, this.columns[2])
+            this.column1Unlock()
         },
-        moveTask(data) {
+        moveTask(data, column) {
             const task = data.column.tasks.splice(data.column.tasks.indexOf(data.task), 1)[0]
-            this.columns[data.column.index + 1].tasks.push(task)
+            column.tasks.push(task)
         },
         column1Unlock() {
             this.columns[0].disabled = false
 
-            this.columns[0].tasks.every(task => {
+            this.columns[0].tasks.forEach(task => {
                 const countSubtaskDone = task.subtasks.filter(subtask => subtask.done).length
                 if (Math.ceil(task.subtasks.length / 2) === countSubtaskDone) {
-                    this.moveTask({task: task, column: this.columns[0]})
+                    this.moveTask({task: task, column: this.columns[0]}, this.columns[1])
                 }
             })
 
+        }
+    },
+    computed: {
+        column1Full() {
+            return this.columns[0].tasks.length === this.columns[0].maxSize
+        },
+        column2Full() {
+            return this.columns[1].tasks.length === this.columns[1].maxSize
         }
     }
 })
